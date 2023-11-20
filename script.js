3670e42925e0a7952e4343487f72a20c0191b1a0
Jharo <10403533+jharoleon@users.noreply.github.com>
2023-11-20 15:42:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+//this function listens to keystrokes and filters the user input with the fruit array in a lower case. 
+input.onkeyup = function search() {
+	let results = [];
+	let userInput = input.value;			
+	if(userInput.length){
+		results = fruit.filter((keyword) => {
+			return keyword.toLowerCase().includes(userInput.toLowerCase());
+		});
+		console.log(results);
+	}
+	showSuggestions(results)
+}
+
+
+function showSuggestions(results) {
+	const dropDown = results.map((inputVal) => {
+	return "<li onclick=useSuggestion(this)>" + inputVal + "</li>"
+	})
+	suggestions.innerHTML = "<ul>" + dropDown.join('') +"</ul>" //.join removes comma
+}
+
+function useSuggestion(e) {
+	input.value = e.innerHTML;
+	suggestions.innerHTML = '';
+}
